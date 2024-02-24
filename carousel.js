feabafd0b61d7fb90f1eda34617b3caa4260afe4
codingbymanish <codingbymanish20@gmail.com>
2024-02-24 13:51:28 +0530

    done

---
+++ carousel.js
@@ -0,0 +1,21 @@
+$('#owlfirst').owlCarousel({
+    loop: true,
+    margin: 10,
+    responsiveClass: true,
+    autoplay: true,
+    responsive: {
+        0: {
+            items: 1,
+            nav: true
+        },
+        600: {
+            items: 3,
+            nav: false
+        },
+        1000: {
+            items: 3,
+            nav: true,
+            loop: true
+        }
+    }
+});
